ff730d67638823f92431237768525e389238e1e3
Daniel Barden <daniel@azapp.net>
2012-07-21 07:21:21 -0300

    Initial Commit

---
+++ BNRRandomPossessions/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  BNRRandomPossessions
+//
+//  Created by Daniel Barden on 7/21/12.
+//  Copyright (c) 2012 Daniel Barden. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
